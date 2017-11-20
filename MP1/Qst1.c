99fb7d0fee18741afd8c7955a93cf687b32d5479
drissireda <mohamedreda.drissi@edu.uca.ma>
2017-11-20 21:17:13 +0100

    Dropped linked list implementation, stupid idea

--- MP1/Qst1.c
+++ MP1/Qst1.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-typedef struct node
+#include <errno.h>
+extern int errno;
+//TODO implement special args array where pipes and redirections are stored
+char** parse(char* buff)
 {
-    char* data;
-    struct node* next;
-} node;
-node* parse(char* buff)
-{
-    char* res;
-    node* ret;
-    node* tmp;
+    char* res;  
     int i=0;
-    ret=(node*)malloc(sizeof(node));
-    tmp=ret;
+    char** ret=(char**)malloc(sizeof(char*)*255);
     while(res=strsep(&buff," "))
     {
-        tmp->data=malloc(strlen(res)+1);
-        strcpy(tmp->data,res);
-        tmp->next=malloc(sizeof(node));
-        tmp=tmp->next;
+        ret[i]=malloc(strlen(res)+1);
+        strcpy(ret[i],res);
         i++;
     }
     return ret;
@@ -34,39 +28,122 @@ void cd(char* dir)
     else
         chdir(dir);
 }
+int find_redir(char** args)
+{
+    int i=0;
+    while(args[i])
+    {
+        if(!strcmp(args[i],"<")||!strcmp(args[i],">")||!strcmp(args[i],"2>"))
+            return i;
+        i++;
+    }
+    return -1;
+}
+void def_cmd(char** args)
+{
+    int i=find_redir(args);
+    int fd;
+    if(i>0)
+    {
+        switch(args[i][0])
+        {
+            case '<' :
+                if((fd=open(args[i+1], O_RDONLY)) < 0)
+                {
+                    perror(args[i+1]);
+                    exit(1);
+                }
+                if(fd)
+                {
+                    dup2(fd,0);
+                    close(fd);
+                }
+                break;
+            case '>' :
+                 if((fd=open(args[i+1], O_WRONLY)) < 0)
+                {
+                    perror(args[i+1]);
+                    exit(1);
+                }
+                 if(fd!=1)
+                {
+                    dup2(fd, 1);
+                    close(fd);
+                }
+                 break;
+            case '2' :
+                 if((fd=open(args[i+1], O_WRONLY)) <0)
+                {
+                    perror(args[i+1]);
+                    exit(1);
+                }
+                 if(fd!=2)
+                {
+                    dup2(fd,2);
+                    close(fd);
+                }
+                 break;
+            }
+        //cleaning
+        int j=i;
+        while(args[j++]);
+        j--;
+        while(j>=i)
+        {
+            free(args[j--]);
+        }
+    }
+    execvp(args[0],args);
+    perror(args[0]);
+    exit(0);
+}
 int main(int argc,char* argv[])
 {
     char* buff=malloc(255);
     char* cwd=malloc(255);
-    node* args;
+    char** args;
     cwd=getcwd(cwd,255);
-    printf("\n%s %% ",cwd);
+    printf("%s %% ",cwd);
     while(fgets(buff,255,stdin)!= NULL)
     {
+
+        buff[strlen(buff)-1]=0; // remove the '\n'
         args=parse(buff);
-        int i=0;
-        node* tmp=args;
-        while(tmp != NULL){
-            printf("%s\n",tmp->data);
-            tmp=tmp->next;
-        }
-        if(args==NULL || !strcmp(args->data,""))
+        if(args==NULL || !strcmp(args[0],""))
         {
-            printf("\n%s %% ",cwd);
+            printf("%s %% ",cwd);
             continue;
         }
-        if(!strcmp(args->data,"exit"))
+        if(!strcmp(args[0],"exit"))
         {
             printf("\n");
             break;
         }
-        if(!strcmp(args->data,"cd"))
+        if(!strcmp(args[0],"cd"))
         {
-            cd(args->next->data);
+            cd(args[1]);
             cwd=getcwd(cwd,255);
-            printf("\n%s %% ",cwd);
+            printf("%s %% ",cwd);
             continue;
         }
-        printf("\n %s %% ",cwd);
+        else
+        {
+        pid_t pid;
+        switch(pid=fork())
+        {
+            case -1 : 
+                perror("fork");
+                errno=0;
+                break;
+            case 0 :
+                def_cmd(args);
+            case 1 :
+                wait(NULL);
+                //printf("%s %%",cwd);
+                break;
+        }
+        printf("%s %%\n",getcwd(cwd,255));
+        }
     }
+    printf("\n");
 }
