c23445aad68025a41f95788ad69cc9939e3ae94f
drissireda <mohamedreda.drissi@edu.uca.ma>
2017-12-01 17:38:14 +0100

    Added header

---
+++ MP1/Mishell.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <readline/readline.h>
+
+#define MAX 1000
+#define HISTMAX 500
+typedef struct piped
+{
+	char** args;
+	struct piped* next;
+} piped;
+typedef struct node
+{
+	int i;
+	struct node* next;
+}node ;
+extern int errno;
+
+void helper();
+void cd(char* directory, char** old_directory);
+void hist();
+void add_hist(char* command, int* index);
+
+
+piped* parse(char* buffer, int* flag);
+
+
+node* find_redir(char** arguments);
+void redir(int oldfd, int newfd);
+void do_exec(int input_fd, int output_fd, char** arguments);
+int spawn_exec(int input_fd,int output_fd, char** arguments);
+int fork_pipes(piped* commands);
+int def_cmd(piped* commands);
+
+
+void clean_piped(piped** commands);
+void clean_node(node** list);
+
